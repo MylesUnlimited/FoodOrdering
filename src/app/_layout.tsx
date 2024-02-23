c3871ac25d7f53edcc1496982987a4365f8876f4
davidkpognon <dskpognon20@students.desu.edu>
2024-02-23 16:37:45 -0500

    changes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,84 +1,84 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from '@expo/vector-icons/FontAwesome'; // imports FontAwesome icon library from Expo
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-import CartProvider from '@/providers/CartProvider';
-import AuthProvider from '@/providers/AuthProvider';
-import QueryProvider from '@/providers/QueryProvider';
-import { StripeProvider } from '@stripe/stripe-react-native';
-import NotificationProvider from '@/providers/NotificationProvider';
+} from '@react-navigation/native'; // import themes and ThemeProvider from React Navigation
+import { useFonts } from 'expo-font'; // import useFonts hook from Expo to load custom fonts
+import { SplashScreen, Stack } from 'expo-router'; // imports SplashScreen and Stack 
+import { useEffect } from 'react'; // import useEffect hook from React
+import { useColorScheme } from 'react-native'; // imports useColorScheme hook 
+import CartProvider from '@/providers/CartProvider'; // imports CartProvider component
+import AuthProvider from '@/providers/AuthProvider'; // import AuthProvider component
+import QueryProvider from '@/providers/QueryProvider'; // imports QueryProvider component
+import { StripeProvider } from '@stripe/stripe-react-native'; // import StripeProvider from 
+import NotificationProvider from '@/providers/NotificationProvider'; // imports NotificationProvider component
 
 export {
-  // Catch any errors thrown by the Layout component.
+  // catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from 'expo-router'; // exporting ErrorBoundary 
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  // ensure that reloading on modal keeps a back button present.
+  initialRouteName: '(tabs)', // setting initial route name for navigation
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// prevent the splash screen from auto hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync(); // preventing splash screen from auto-hiding 
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'), // loading custom font SpaceMono
+    ...FontAwesome.font, // loading FontAwesome font
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  // expo Router uses Error Boundaries to catch errors 
   useEffect(() => {
-    if (error) throw error;
+    if (error) throw error; // throw error if there's an error during font loading
   }, [error]);
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync(); // hide splash screen when fonts are loaded
     }
   }, [loaded]);
 
   if (!loaded) {
-    return null;
+    return null; // Render null if fonts are not yet loaded
   }
 
-  return <RootLayoutNav />;
+  return <RootLayoutNav />; // render RootLayoutNav component when fonts are loaded
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme(); // get the color scheme of the device
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> 
       <StripeProvider
-        publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY || ''}
+        publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY || ''} // Provide Stripe publishable key
       >
-        <AuthProvider>
-          <QueryProvider>
-            <NotificationProvider>
-              <CartProvider>
-                <Stack>
+        <AuthProvider> // Provide authentication context
+          <QueryProvider> // Provide data query context
+            <NotificationProvider> // Provide notification context
+              <CartProvider> // Provide shopping cart context
+                <Stack> // Render stack navigator
                   <Stack.Screen
                     name="(admin)"
-                    options={{ headerShown: false }}
+                    options={{ headerShown: false }} // hide header for admin screen
                   />
                   <Stack.Screen
                     name="(user)"
-                    options={{ headerShown: false }}
+                    options={{ headerShown: false }} // hide header for user screen
                   />
                   <Stack.Screen
                     name="(auth)"
-                    options={{ headerShown: false }}
+                    options={{ headerShown: false }} // hide header for authentication screen
                   />
                   <Stack.Screen
                     name="cart"
-                    options={{ presentation: 'modal' }}
+                    options={{ presentation: 'modal' }} // set cart screen to be presented as a modal
                   />
                 </Stack>
               </CartProvider>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,32 @@
-import { View, Text, ActivityIndicator } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link, Redirect } from 'expo-router';
-import { useAuth } from '@/providers/AuthProvider';
-import { supabase } from '@/lib/supabase';
+import { View, Text, ActivityIndicator } from 'react-native'; // import components 
+import React from 'react'; // import React
+import Button from '../components/Button'; // import Button component 
+import { Link, Redirect } from 'expo-router'; // imports Link and Redirect 
+import { useAuth } from '@/providers/AuthProvider'; // imports useAuth hook 
+import { supabase } from '@/lib/supabase'; // import supabase object 
 
-const index = () => {
-  const { session, loading, isAdmin } = useAuth();
+const index = () => { // defines index functional component
+  const { session, loading, isAdmin } = useAuth(); // destructs session, loading, and isAdmin from useAuth hook
 
-  if (loading) {
-    return <ActivityIndicator />;
+  if (loading) { // checks if loading is true
+    return <ActivityIndicator />; // rendering activityIndicator component if loading is true
   }
 
-  if (!session) {
-    return <Redirect href={'/sign-in'} />;
+  if (!session) { // checking if there's no active session
+    return <Redirect href={'/sign-in'} />; // Redirecting to the sign-in page 
   }
 
-  if (!isAdmin) {
-    return <Redirect href={'/(user)'} />;
+  if (!isAdmin) { // checking if the user is not an admin
+    return <Redirect href={'/(user)'} />; // redirecting to the user page 
   }
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
-        <Button text="User" />
+  return ( // renders the UI components
+    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}> 
+      <Link href={'/(user)'} asChild> 
+        <Button text="User" /> 
       </Link>
-      <Link href={'/(admin)'} asChild>
-        <Button text="Admin" />
+      <Link href={'/(admin)'} asChild> 
+        <Button text="Admin" /> 
       </Link>
 
       <Button onPress={() => supabase.auth.signOut()} text="Sign out" />
@@ -34,4 +34,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default index; // exporting index component as default
