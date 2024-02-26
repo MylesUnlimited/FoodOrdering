fd33f48a41038cb64924cdd4ff25cab4aa5870f5
rhltray <75441694+rhltray@users.noreply.github.com>
2024-02-26 13:49:59 -0500

    Update profile.tsx

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,17 +1,25 @@
+// Importing supabase object from the specified path
 import { supabase } from '@/lib/supabase';
+
+// Importing View, Text, and Button components from react-native
 import { View, Text, Button } from 'react-native';
 
+// ProfileScreen function component
 const ProfileScreen = () => {
   return (
+    // View component to contain profile information
     <View>
+      {/* Text component displaying 'Profile' */}
       <Text>Profile</Text>
 
+      {/* Button component to sign out */}
       <Button
-        title="Sign out"
-        onPress={async () => await supabase.auth.signOut()}
+        title="Sign out" // Button title
+        onPress={async () => await supabase.auth.signOut()} // Sign out action
       />
     </View>
   );
 };
 
+// Default export for the ProfileScreen component
 export default ProfileScreen;
