9f5056ccaa5a9baba6f9484b76b9fe16348a56ba
rhltray <75441694+rhltray@users.noreply.github.com>
2024-02-26 13:45:44 -0500

    Update index.tsx

--- src/app/(user)/index.tsx
+++ src/app/(user)/index.tsx
@@ -1,5 +1,8 @@
+// Importing Redirect component from expo-router
 import { Redirect } from 'expo-router';
 
+// Default export for the TabIndex function component
 export default function TabIndex() {
+  // Redirecting to the specified route
   return <Redirect href={'/(user)/menu/'} />;
 }
