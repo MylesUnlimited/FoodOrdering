9e35a4fffc9b02ada3a63ff14d7474fdf51830cd
rhltray <75441694+rhltray@users.noreply.github.com>
2024-02-26 10:47:55 -0500

    Update _layout.tsx

--- src/app/(user)/menu/_layout.tsx
+++ src/app/(user)/menu/_layout.tsx
@@ -1,15 +1,27 @@
+// Importing Colors module from the specified path
 import Colors from '@/constants/Colors';
+
+// Importing FontAwesome component from Expo vector-icons
 import { FontAwesome } from '@expo/vector-icons';
+
+// Importing Link and Stack components from expo-router
 import { Link, Stack } from 'expo-router';
+
+// Importing Pressable component from react-native
 import { Pressable } from 'react-native';
 
+// Default export for the MenuStack function component
 export default function MenuStack() {
   return (
+    // Stack component with screenOptions for navigation configuration
     <Stack
       screenOptions={{
+        // Configuring headerRight with a Link component leading to the cart screen
         headerRight: () => (
           <Link href="/cart" asChild>
+            {/* Pressable component to handle touch feedback */}
             <Pressable>
+              {/* FontAwesome icon representing a shopping cart */}
               {({ pressed }) => (
                 <FontAwesome
                   name="shopping-cart"
@@ -23,6 +35,7 @@ export default function MenuStack() {
         ),
       }}
     >
+      {/* Screen configuration for the index screen with the title 'Menu' */}
       <Stack.Screen name="index" options={{ title: 'Menu' }} />
     </Stack>
   );
